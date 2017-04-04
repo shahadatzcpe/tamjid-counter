a977105e77a82cc5d5d4af73b7cf47634b31b040
Mohammad Shahadat Hossain <shahadatzcpe@users.noreply.github.com>
2017-04-05 00:05:34 +0600

    Create tamjid-counter.js

---
+++ src/tamjid-counter.js
@@ -0,0 +1,64 @@
+(function($) {
+
+    "use strict";
+
+    $.fn.tamjidCounter = function(options) {
+
+        var opts = $.extend({}, $.fn.tamjidCounter.defaults, options);
+
+
+        return this.each(function() {
+
+            var $element = $(this);
+
+            $(document).ready(function() {
+                $.fn.tamjidCounterPerform($element, opts);
+            });
+
+
+            $(document).on('scroll', function() {
+                $.fn.tamjidCounterPerform($element, opts);
+            });
+
+        });
+
+    };
+
+    $.fn.tamjidCounterPerform = function($element, options) {
+
+
+        var offset = $element.offset();
+        var screenHeight = $(window).height();
+        var focus_form_top = offset.top < $(document).scrollTop() + screenHeight;
+        var focus_form_bottom = offset.top + $element.height() >= $(document).scrollTop();
+        var already_countered = $element.hasClass('tamjid-performed');
+
+        if (focus_form_top && focus_form_bottom && !already_countered) {
+
+            setTimeout(function() {
+
+                $element.prop('Counter', 0).animate({
+                    Counter: $element.text()
+                }, {
+                    duration: options.duration,
+                    easing: options.easing,
+                    step: function(now) {
+                        $element.text(Math.ceil(now));
+                    }
+                });
+
+                $element.addClass('tamjid-performed');
+
+            }, options.delay);
+
+        }
+
+    };
+
+    $.fn.tamjidCounter.defaults = {
+        duration: 4000,
+        delay: 0,
+        easing: 'swing'
+    };
+
+})(jQuery);
